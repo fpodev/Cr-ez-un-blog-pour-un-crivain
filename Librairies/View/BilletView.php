103f5aa315c8b6657c0bbc7b0c40c6dd9f4ca3fd
fpodev <fpo@gmx.fr>
2020-02-29 20:11:13 +0100

    suppresion d'élément pour gabarit

--- Librairies/View/BilletView.php
+++ Librairies/View/BilletView.php
@@ -1,69 +1,68 @@
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-  <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Billet</title>  
-    <link rel="stylesheet" type="text/css" href="style/admin.css">
-  </head>  
-  <body>
-  <p><a href="index.php">Accueil</a></p>
 <?php
+$titre = 'Article';
+require 'Librairies/Template/Head.php';
+?>
+<body id="billet"> 
+  <div class='page'>
+    <div class='contenaire'>
+<?php require 'Librairies/Template/Header.php';
+
  foreach($billetId as $billet):
-  echo '<h2>', $billet->titre(), '</h2>', "\n",
-       '<p>', nl2br($billet->contenu()), '</p>', "\n";
+  echo '<article class=billet>
+        <h2>', $billet->titre(), '</h2>', "\n",
+        '<p class="contenu">', nl2br($billet->contenu()), '</p>', "\n";
   
   if (date('d/m/y à H:i:s', strtotime($billet->dateAjout())) === date('d/m/y à H:i:s', strtotime($billet->dateModif())))
   {
-    echo  '<p><em>Ajouter le ', date('d/m/y à H:i:s', strtotime($billet->dateAjout())), '</em></small></p>';
+    echo  '<p class="date"><em>Ajouter le ', date('d/m/y à H:i:s', strtotime($billet->dateAjout())), '</em></small></p>';
   }
   else
   {   
-    echo  '<p><em>Modifiée le ', date('d/m/y à H:i:s', strtotime($billet->dateModif())), '</em></small></p>';
+    echo  '<p class="date"><em>Modifiée le ', date('d/m/y à H:i:s', strtotime($billet->dateModif())), '</em></small></p>';
   }
- endforeach;
-?>
+  echo '</article>';
+ endforeach; 
+?>  
 <?php 
+  if($commentList == true)
+  {
+  echo '<p class="comTitre">Commentaire(s)</p>';
   foreach($commentList as $comment): 
-    echo //'<p>', $comment->count(),'</p><',
-         '<p>', $comment->pseudo(),'</p>',
-         '<p><em>', date('d/m/y à H:i:s', strtotime($comment->dateAjout())),'</em></small></p>',
-         '<p>', $comment->contenu(),'</p>';
+     echo'<div class="commentaire">
+         <p>De<span class="pseudo"> ' .$comment->pseudo(). ' </span><small><em>le ', date('d/m/y à H:i:s', strtotime($comment->dateAjout())),'</em></small></p>';
+         
          if($comment->signaler() === 'signaler')
          {
-         echo '<p>Commentaire en cour de modération</p>';         
+         echo '<p class="textSignal"><small>Commentaire en cour de modération</small></p>';         
          }
          else
          {
-         echo '<p><a href="index.php?signal=',$comment->id(),'">Signaler ce commentaire</a></p>'; 
-             
-         } 
-        echo '<p>---------------------------------------------------------------------------------</p>';               
-         
-
+         echo '<p class="textComment">', $comment->contenu(),'</p> 
+               <p class="textSignal"><a href="index.php?signalComment=',$comment->id(),'& idBillet=',$billet->id(),'"><small>Signaler ce commentaire</small></a></p>';              
+         }
+         echo '</div>';                      
         endforeach; 
-?>  
-<?php  
-if (isset($message))
-{
-  echo $message, '<br />';
-} 
+  }       
 ?>
-<form action='index.php' method="post"> 
-       
-        <label for="pseudo">Pseudo :</label>
-        <input type="text" name="pseudo" require /> 
-       
-        <label for="mail">Votre adresse mail :</label>
-        <input type="email" name="mail" require />
-       
-        <label for="contenu">Votre commentaire :</label>
-        <textarea type='text' name="contenu" require ></textarea> 
+      <div class="commentForm">  
+        <p> Ajouter un Commentaire</p>
+        <form action='index.php' method="post"> 
+              
+                <label for="pseudo">Pseudo :</label> <br>
+                <input type="text" name="pseudo" require /> <br>
+              
+                <label for="mail">Votre adresse mail :</label> <br>
+                <input type="email" name="mail" require />  <br>
+              
+                <label for="contenu">Votre commentaire :</label> <br>
+                <textarea type='text' name="contenu" require ></textarea> <br>
 
-        <input type='hidden' name='id_billet' value='<?=$billet->id()?>'/>                     
-        <input type="submit" name="action" value="Envoyer"/>   
-        </form>  
-        <?= require_once('Librairies/Template/Footer.php')?>            
+                <input type='hidden' name='id_billet' value='<?=$billet->id()?>'/>                     
+                <input type="submit" name="action" value="Envoyer"/>               
+        </form> 
+      </div>
+  </div> 
+<?php include 'Librairies/Template/Footer.php';?>  
+    </div>         
   </body>  
 </html>
