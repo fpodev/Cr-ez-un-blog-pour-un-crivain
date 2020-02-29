d5a9a5fa5486cc9e963a5fb74208d3e4af1e64d9
fpodev <fpo@gmx.fr>
2020-02-29 20:12:13 +0100

    divers suppression

--- Librairies/View/HomeView.php
+++ Librairies/View/HomeView.php
@@ -1,36 +1,27 @@
 <?php
-use App\Model\BilletsManager;
-?>
-<!DOCTYPE html>
-<html lang="fr">
-  <head>
-  <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>administration</title>  
-    <link rel="stylesheet" type="text/css" href="style/admin.css">
-  </head>
-  
-  <body>
-    <p><a href="<?="index.php?action=admin"?>">Accéder à l'espace d'administration</a></p>
-<?php
-  echo '<h2 style="text-align:center">Liste des 5 dernières news</h2>';
-
- foreach ($billetList as $billet)  
+ $titre = 'Accueil';
+ require 'Librairies/Template/Head.php';
+?> 
+<body class="homePage">
+  <div class="page"> 
+    <div class="contenaire">
+<?php require 'Librairies/Template/Header.php';
+     
+ foreach ($billetsList as $billet)  
   {
-    if (strlen($billet->contenu()) <= 200)
+   if (strlen($billet->contenu()) <= 400)
     {
       $contenu = $billet->contenu();      
     }   
     else
     {
-      $debut = substr($billet->contenu(), 0, 200);
+      $debut = substr($billet->contenu(), 0, 400);
       $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
       
       $contenu = $debut;
-    }  
-    echo '<h4><a href="index.php?id='.$billet->id(), '">', $billet->titre(), '</a></h4>', "\n",
-         '<p>', nl2br($contenu), '</p>';
+    }   
+        echo '<article class="billetList">
+         <h2>', $billet->titre(), '</h2>', "\n";        
          if (date('d/m/y à H:i:s', strtotime($billet->dateAjout())) != date('d/m/y à H:i:s', strtotime($billet->dateModif())))
          {
           echo '<p><em>Modifier le', date('d/m/y à H:i:s', strtotime($billet->dateModif())), '</em></small></p>';
@@ -38,9 +29,28 @@ use App\Model\BilletsManager;
          else
          {
            echo '<p><em>Ajouter le ', date('d/m/y à H:i:s', strtotime($billet->dateAjout())), '</em></small></p>';
-         }         
- }
-  
+         } 
+        echo '<p class="contenu">', nl2br($contenu), ' <a href="index.php?billetUnique='.$billet->id(), '">Lire la suite</a></p>
+         
+          </article>';                   
+        } 
 ?>
-  </body>
+      <div class='navBar'>
+      <?php if($pageCourante == 1)
+            { 
+               echo 'page ',$pageCourante,'/', $nbPages , ' <a href="?page=', $pageCourante + 1 , '">suivante</a>';
+            }                           
+            elseif($pageCourante == $nbPages)
+            {
+               echo '<a href="?page=', $pageCourante - 1 ,'">précédente</a> page ', $pageCourante,'/', $nbPages ;
+            }
+            else
+            {              
+               echo '<a href="?page=', $pageCourante - 1 ,'">précédente</a> page ',  $pageCourante,'/', $nbPages , ' <a href="?page=', $pageCourante + 1 ,'">suivante</a>';
+            }        
+      echo '</div>
+            </div>';  
+     require 'Librairies/Template/Footer.php';?>  
+      </div>
+  </body>      
 </html>
