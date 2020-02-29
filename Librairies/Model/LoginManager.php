3730546a58d2622be3ceaa871351b4b6d2b7538d
fpodev <fpo@gmx.fr>
2020-02-29 20:05:21 +0100

    creation

---
+++ Librairies/Model/LoginManager.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Model;
+use PDO;
+
+class LoginManager
+{
+    private $db;
+
+    public function __construct(PDO $db)
+    {
+        $this->db = $db;
+    }
+    public function connexion($identifiant)
+    {
+        $q = $this->db->prepare('SELECT pass FROM connexion WHERE identifiant = :identifiant');        
+       
+        $q->bindValue(':identifiant', $identifiant);                
+
+        $q->execute();  
+        
+        $resultat = $q->fetch();
+
+        return $resultat;         
+    }    
+    public function nouveauPass($identifiant){
+        
+        $pass_hache = password_hash($_POST['pass2'], PASSWORD_DEFAULT);
+         
+        $q = $this->db->prepare('UPDATE connexion SET pass = :pass WHERE identifiant = :identifiant');       
+        
+        $q->bindValue(':pass', $pass_hache);
+        $q->bindValue(':identifiant', $identifiant);
+
+        $q->execute();
+
+    }   
+}
