f1fec9f9642dce8df113c7c68aa12973deba81ad
fpodev <fpo@gmx.fr>
2020-02-29 20:08:55 +0100

    corection erreur et ajout copyright

--- Librairies/Template/Footer.php
+++ Librairies/Template/Footer.php
@@ -2,7 +2,10 @@
  <div>
  <p>Contact: </p>
  </div>
+<div>
+<p><a href="<?="index.php?connexion"?>">privé</a></p>    
+</div>
  <div> 
- <p>Copyright <?=$date= date('Y')?></p>
- </div>
- </footer
+ <p>Copyright© fpodev, projet openclass rooms - <?=$date= date('Y')?></p>
+ </div> 
+</footer>
