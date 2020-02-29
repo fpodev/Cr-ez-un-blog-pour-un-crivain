1ba75a605eb195ac2c65fba4d7cf080399b084fd
fpodev <fpo@gmx.fr>
2020-02-29 20:11:42 +0100

    création

---
+++ Librairies/View/Erreur404.php
@@ -0,0 +1,14 @@
+<?php
+
+$titre = '404';
+require 'Librairies/Template/Head.php';
+?>
+<body>
+    <div id=erreur>
+    <p>Oups.....erreur 404!</p> 
+    <p>Page non trouvée</p>
+    <button><a href="index.php">Revenir à l'accueil</a></button>
+</div>
+</body>
+</html>
+
