e1638bef4ea7cab2d50c1e17c4232d7b3a90690c
fpodev <fpo@gmx.fr>
2020-03-02 17:27:14 +0100

    Update Connect.php

--- Librairies/Objet/Connect.php
+++ Librairies/Objet/Connect.php
@@ -5,7 +5,7 @@ use PDO;
 class Connect
 {    
     public static function getPDO() {       
-        $db = new PDO('mysql:host=fpodevovly796.mysql.db;dbname=fpodevovly796;charset=utf8','fpodevovly796', 'PzyfeeaGkbfK5');
+        $db = new PDO('mysql:host="dbname=";charset=utf8','"', '"K5');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
    
