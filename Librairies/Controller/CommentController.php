3fedd6081941dadc972c54ec6fe84df9306f830c
fpodev <fpo@gmx.fr>
2020-03-06 18:41:23 +0100

    ventilation

--- Librairies/Controller/CommentController.php
+++ Librairies/Controller/CommentController.php
@@ -9,28 +9,29 @@ use App\Controller\BilletsController;
 class CommentController{
 
     public function save(){    
-      $db = Connect::getPDO(); 
-      $manager = new CommentManager($db);
-      $commentaires = new Commentaires(             
-      [                          
-           'pseudo' => $_POST['pseudo'],
-           'mail' => $_POST['mail'],
-           'contenu' => $_POST['contenu'],
-           'id_billet' => (int)$_POST['id_billet']           
-      ]
-      );                         
-        if($commentaires->isValid())
-        {     
-          $manager->save($commentaires) ;                               
-        }
-        else  
-        {
-          $erreurs = $commentaires->erreurs();
-        }              
-      $billet = $commentaires->id_billet();   
-      
-      header("Location: index.php?billetUnique=$billet");              
-    }                 
+        $db = Connect::getPDO(); 
+        $manager = new CommentManager($db);
+        $commentaires = new Commentaires(             
+        [                          
+            'pseudo' => $_POST['pseudo'],
+            'mail' => $_POST['mail'],
+            'contenu' => $_POST['contenu'],
+            'id_billet' => (int)$_POST['id_billet']           
+        ]
+        );                         
+          if($commentaires->isValid())
+          {     
+            $manager->save($commentaires) ;                               
+          }
+          else  
+          {
+            $erreurs = $commentaires->erreurs();
+          }              
+        $billet = $commentaires->id_billet();   
+        
+        header("Location: index.php?billetUnique=$billet");              
+    }
+
     public function signaler(){
         $db = Connect::getPDO(); 
         $manager = new CommentManager($db);  
@@ -47,13 +48,15 @@ class CommentController{
         $adminPage= new BilletsController();
         $adminPage->adminList();
 
-    }
+    }      
     public function delete(){
-      $db = Connect::getPDO();
-      $manager = new CommentManager($db);
-      $delete = $manager->delete((int)$_GET['deleteComment']);  
-      
-      header("Location: index.php?connexion");
+
+        $db = Connect::getPDO();
+        $manager = new CommentManager($db);
+
+        $delete = $manager->delete((int)$_GET['deleteComment']);  
+        
+        header("Location: index.php?connexion");
     }
  } 
 ?>  
