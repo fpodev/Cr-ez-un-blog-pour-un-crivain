ebb88d2076db3cf5a7a5034627a10912330f9b33
fpodev <fpo@gmx.fr>
2020-03-07 19:29:40 +0100

    modif erreur

--- Librairies/Objet/Commentaires.php
+++ Librairies/Objet/Commentaires.php
@@ -3,20 +3,13 @@ namespace App\Objet;
 
 class Commentaires
 {
-    private $erreur = [];
     private $id;  
     private $pseudo;
     private $mail;
     private $contenu;
     private $id_billet;
     private $dateAjout; 
-    private $signaler;
-
-    const ID_BILLET_INVALIDE = 1;
-    const PSEUDO_INVALIDE = 2;
-    const CONTENU_INVALIDE = 3;
-    const MAIL_INVALIDE = 4;
-    const SIGNALER_INVALIDE = 5;
+    private $signaler;   
 
     public function __construct($valeurs=[])
     {
@@ -37,10 +30,7 @@ class Commentaires
             }
         endforeach;
     }
-    public function isNew()
-    {
-        return empty($this->id);
-    }      
+         
     public function isValid()
     {
         return !(empty($this->id_billet) || empty($this->pseudo) || empty($this->contenu) || empty($this->mail));
@@ -51,64 +41,28 @@ class Commentaires
         $this->id = (int) $id;
     } 
     public function setId_billet($id_billet)    
-    {
-        if (!is_int($id_billet) || empty($id_billet))
-        {
-            $this->erreurs[] = self::ID_BILLET_INVALIDE;
-        }
-        else
-        {
-            $this->id_billet = (int) $id_billet;
-        }          
+    {             
+        $this->id_billet = (int) $id_billet;                 
     } 
     public function setPseudo($pseudo)
-    {
-        if (!is_string($pseudo) || empty($pseudo))
-        {
-            $this->erreurs[] = self::PSEUDO_INVALIDE;
-        }
-        else
-        {
-            $this->pseudo = $pseudo;
-        }
+    {        
+        $this->pseudo = $pseudo;        
     }
     public function setContenu($contenu)
     {
-        if (!is_string($contenu) || empty($contenu))
-        {
-            $this->erreur[] = self::CONTENU_INVALIDE;
-        }
-        else
-        {
-            $this->contenu = $contenu;
-        }
+        $this->contenu = $contenu;        
     }
     public function setMail($mail)
-    {
-        if (!is_string($mail) || empty($mail))
-        {
-            $this->erreurs[] = self::MAIL_INVALIDE;
-        }
-        else
-        {
-            $this->mail = $mail;
-        }
+    {        
+        $this->mail = $mail;        
     }
     public function setDateAjout(DateTime $dateAjout)
     {
         $this->dateAjout = $dateAjout;
     }
-   public function setSignaler()
-    {
-        
-            if (!is_string($signaler) || empty($signaler))
-            {
-                $this->erreurs[] = self::SIGNALER_INVALIDE;
-            }
-            else
-            {
-                $this->signaler = $signaler;
-            }         
+    public function setSignaler()
+    {               
+        $this->signaler = $signaler;                     
     }
     //getters
     public function erreurs()
