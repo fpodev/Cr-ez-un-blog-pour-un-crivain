909db4a71b320a518141847dc2885a3def0e9fbd
fpodev <fpo@gmx.fr>
2020-03-07 11:09:05 +0100

    modif pour faclitité l'affichage

--- Librairies/View/AdminChange.php
+++ Librairies/View/AdminChange.php
@@ -1,9 +1,7 @@
-<?php
+<?php use App\Model\BilletsManager; 
 $titre = 'administration'; 
 require 'Librairies/Template/Head.php';
-?>
-<body>
-<?php
+
         echo '<h1 class="titreAdmin">Bienvenue sur votre page d\'administration</h1>
               <button class="btnAdmin"><a href="index.php?Accueil">Retour à l\'accueil</a></button>  
               <button class="btnAdmin"><a href="index.php?changePass">Changer votre mot de passe</a></button>
@@ -21,7 +19,7 @@ require 'Librairies/Template/Head.php';
         {
         ?>
                 <input type="hidden" name="id" value="<?= $billet->id() ?>" />
-                <input class="btnForm" type="submit" value="Modifier" name="postModifier" />
+                <input class="btnForm" type="submit" value="Valider" name="postModifier" />
                 <input class="btnForm" type="submit" value="Annuler" name="annuler"/>        
         <?php
         }
@@ -33,23 +31,12 @@ require 'Librairies/Template/Head.php';
         }         
         echo ' </p>
          </form>
-        </div>
+        </div>'        
         
-        <p class="listeAdmin">Il y a actuellement '. $billetCount .' billets. En voici la liste :</p>
-
-        <table>
-            <tr><th>Titre</th><th>Date d\'ajout</th><th>Dernière modification</th><th>Action</th></tr>';
-   
-    foreach($billetList as $billets){
-            $date1 = date('d/m/y à H:i:s', strtotime($billets->dateAjout()));
-            $date2 = date('d/m/y à H:i:s', strtotime($billets->dateModif()));      
-            echo '<tr><td>',$billets->titre(), '</td><td>', $date1, '</td><td>', ($date1 == $date2 ? '-' : $date2),
-                '</td><td><button class="btnModif"><a href="index.php?modifierBillet='. $billets->id(),  '">Modifier</a></button> | <button class="delete"><a href="index.php?supprimerBillet='. $billets->id(), '">Supprimer</a></button></td></tr>', "\n";
-        }             
-?>   
-    </table> 
+      ?>      
     <script src="https://cdn.tiny.cloud/1/5ya0hoc0tjh102vqr3m520w8306eqxcu8mz71btr0zmc1z2t/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
-    <script>tinymce.init({selector: '#formulaire'});</script>       
-</body>
-</html>     
+    <script>tinymce.init({selector: '#formulaire'});</script>    
+ 
+
+   
 
