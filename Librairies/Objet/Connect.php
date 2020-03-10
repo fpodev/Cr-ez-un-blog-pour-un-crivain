60b0bbe9badc1da0be17a553e93d866fba9329ba
fpodev <fpo@gmx.fr>
2020-03-10 17:13:04 +0100

    change username et pass

--- Librairies/Objet/Connect.php
+++ Librairies/Objet/Connect.php
@@ -5,7 +5,7 @@ use PDO;
 class Connect
 {    
     public static function getPDO() {       
-        $db = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8','fabrice', 'Frbrl7C90848467');
+        $db = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8','"', '""');
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);       
    
        return $db;
