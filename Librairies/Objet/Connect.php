860d4bdee5d658ce58eddab47293f456031f37e4
fpodev <fpo@gmx.fr>
2020-03-10 17:11:55 +0100

    modif attribut

--- Librairies/Objet/Connect.php
+++ Librairies/Objet/Connect.php
@@ -5,9 +5,8 @@ use PDO;
 class Connect
 {    
     public static function getPDO() {       
-        $db = new PDO('mysql:host="dbname=";charset=utf8','"', '"K5');
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $db = new PDO('mysql:host=localhost;dbname=Blog;charset=utf8','fabrice', 'Frbrl7C90848467');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);       
    
        return $db;
     }   
